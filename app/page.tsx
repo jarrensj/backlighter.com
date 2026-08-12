f779196a21ff36ff1bb4205760933913b818ade2
Jarren San Jose <sanjose.jarren@gmail.com>
2026-08-12 01:13:01 -0700

    Make backlighter an h1 with the iOS app link as a subheader

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ export default function Home() {
   const appStoreUrl = process.env.APP_STORE_URL;
   return (
     <main className="flex flex-1 flex-col items-center justify-center gap-2">
-      backlighter
+      <h1 className="text-4xl font-semibold tracking-tight">backlighter</h1>
       {appStoreUrl && (
-        <a href={appStoreUrl} className="hover:underline">
+        <a
+          href={appStoreUrl}
+          className="text-lg text-neutral-500 hover:underline"
+        >
           download the iOS app
         </a>
       )}
